afa54d80cd17a719a885fb0a9b7acfa504fbe228
Romeo Palacios <romeopalacios@ymail.com>
2017-04-26 22:28:00 -0700

    added tags and added state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,9 +14,13 @@
          })
          .state('album', {
              url: '/album',
-             templateUrl: '/templates/album.html'
+             templateUrl: '/templates/album.html'     
+         })
+          .state('collection', {
+             url: '/collection',
+             templateUrl: 
+             '/templates/collection.html',
          });
-         
      }
  
      angular
