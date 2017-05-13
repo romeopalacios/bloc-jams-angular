48faf97d15051d038f502942ea77c120eda94ed6
Romeo Palacios <romeopalacios@ymail.com>
2017-05-12 17:29:35 -0700

    working seekbar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function () {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
 // Private attributes
@@ -32,6 +32,12 @@
         formats: ['mp3'],
         preload: true
       });
+        
+      currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
 
       SongPlayer.currentSong = song;
     };
@@ -62,6 +68,12 @@
 // Public attributes
     
     SongPlayer.currentSong = null;
+     /**
+      * @desc Current playback time (in seconds) of currently playing song
+      * @type {Number}
+      */
+    SongPlayer.currentTime = null;
+  
     
 // Public methods
       
@@ -130,11 +142,22 @@
         playSong(song);
       }
     };
+      
+       /**
+      * @function setCurrentTime
+      * @desc Set current time (in seconds) of currently playing song
+      * @param {Number} time
+      */
+    SongPlayer.setCurrentTime = function(time) {
+       if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+      }
+   };
     
     return SongPlayer;
   }
-
+    
   angular
     .module('blocJams')
-    .factory('SongPlayer', ['Fixtures', SongPlayer]);
+    .factory('SongPlayer', ['$rootScope','Fixtures', SongPlayer]);
 })();
